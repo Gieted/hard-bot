902935fc62655624836f49138190283c78ae2640
Gieted <gtddd23@gmail.com>
2020-03-08 23:21:52 +0100

    Usuń firestore

--- src/resources/firestore.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase-admin'
-import { firebaseServiceAccount } from './config'
-
-firebase.initializeApp({
-  credential: firebase.credential.cert(firebaseServiceAccount)
-})
-
-export default firebase.firestore()
